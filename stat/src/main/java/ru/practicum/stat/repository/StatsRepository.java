fedeeb8a3a424f23a7bf4a845b84ba9d65df8900
igorekSSEUdet <korablev_sseu@mail.ru>
2023-07-16 15:04:20 +0000

    fix : order of requests

--- stat/src/main/java/ru/practicum/stat/repository/StatsRepository.java
+++ stat/src/main/java/ru/practicum/stat/repository/StatsRepository.java
@@ -10,30 +10,30 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<Stat, Long> {
     @Query("SELECT new ru.practicum.stats.StatOutDto(s.app, s.uri, count(s.id)) " +
-            " FROM Stat as s " +
-            " WHERE s.timestamp BETWEEN :start AND :end " +
-            " GROUP BY s.app, s.uri "
-    )
+            "FROM Stat s " +
+            "WHERE s.timestamp BETWEEN :start AND :end " +
+            "GROUP BY s.app, s.uri " +
+            "ORDER BY count(s.id) DESC")
     List<StatOutDto> countByTimestamp(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.stats.StatOutDto(s.app, s.uri, count(s.id)) " +
-            " FROM Stat as s " +
-            " WHERE s.timestamp BETWEEN :start AND :end " +
-            " GROUP BY s.app, s.uri, s.ip "
-    )
+            "FROM Stat s " +
+            "WHERE s.timestamp BETWEEN :start AND :end " +
+            "GROUP BY s.app, s.uri, s.ip " +
+            "ORDER BY count(s.id) DESC")
     List<StatOutDto> countByTimestampUniqueIp(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.stats.StatOutDto(s.app, s.uri, count(s.id)) " +
-            " FROM Stat as s " +
-            " WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
-            " GROUP BY s.app, s.uri "
-    )
+            "FROM Stat s " +
+            "WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
+            "GROUP BY s.app, s.uri " +
+            "ORDER BY count(s.id) DESC")
     List<StatOutDto> countByTimestampAndList(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.stats.StatOutDto(s.app, s.uri, count(s.id)) " +
-            " FROM Stat as s " +
-            " WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
-            " GROUP BY s.app, s.uri, s.ip "
-    )
+            "FROM Stat s " +
+            "WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
+            "GROUP BY s.app, s.uri, s.ip " +
+            "ORDER BY count(s.id) DESC")
     List<StatOutDto> countByTimestampAndListUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
